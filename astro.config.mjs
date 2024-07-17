36dd4f6454c70e9285d116910086f61fba0c595c
yangqiuyi <yangqiuyi@cn.nutstore.net>
2024-07-17 10:11:50 +0800

    init

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex"
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	markdown: {
+		remarkPlugins: [
+			remarkMath
+		],
+		rehypePlugins: [
+			rehypeKatex
+		]
+	},
+	integrations: [mdx(), sitemap()],
+});
---
+++ src/pages/index.md
@@ -0,0 +1,8 @@
+---
+title: 'test math'
+description: ''
+pubDate: '2024/07/17'
+---
+
+
+$\frac{a}{b}$
